994ec79ec06f13374dc7cc3f47134a592b786ec8
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2023-12-06 13:19:56 +0100

    NavBar

--- src/journal/index.js
+++ src/journal/index.js
@@ -1,2 +1,4 @@
-export * from './pages/JournalPages';
-export * from './routes/JournalRoutes';
+export * from './components';
+export * from './layout';
+export * from './pages';
+export * from './routes';
--- src/journal/pages/JournalPages.jsx
+++ src/journal/pages/JournalPages.jsx
@@ -1,9 +1,17 @@
 import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout';
 
 export const JournalPages = () => {
 	return (
-		<>
-			<Typography variant='h1'>JournalPages</Typography>
-		</>
+		<JournalLayout>
+			<Typography>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+				repudiandae inventore doloribus pariatur eius nemo necessitatibus ea
+				sint sed quo quisquam, nihil, ipsa amet. Sequi eos pariatur veniam. At,
+				necessitatibus.
+			</Typography>
+			{/* NothinSelected */}
+			{/* NoteView */}
+		</JournalLayout>
 	);
 };
