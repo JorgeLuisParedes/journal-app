1abab5152d633a33ebaa8316be177ce6b33f692b
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2023-12-06 15:13:02 +0100

    Boton Flotante

--- src/journal/pages/JournalPages.jsx
+++ src/journal/pages/JournalPages.jsx
@@ -1,5 +1,7 @@
+import { IconButton } from '@mui/material';
 import { JournalLayout } from '../layout';
 import { NoteView, NothingSelectedView } from '../views';
+import { AddOutlined } from '@mui/icons-material';
 
 export const JournalPages = () => {
 	return (
@@ -10,8 +12,21 @@ export const JournalPages = () => {
 				sint sed quo quisquam, nihil, ipsa amet. Sequi eos pariatur veniam. At,
 				necessitatibus.
 			</Typography> */}
-			{/* <NothingSelectedView /> */}
-			<NoteView />
+			<NothingSelectedView />
+			<IconButton
+				size='large'
+				sx={{
+					color: 'white',
+					backgroundColor: 'error.main',
+					':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+					position: 'fixed',
+					right: 50,
+					bottom: 50,
+				}}
+			>
+				<AddOutlined sx={{ fontSize: 30 }} />
+			</IconButton>
+			{/* <NoteView /> */}
 		</JournalLayout>
 	);
 };
