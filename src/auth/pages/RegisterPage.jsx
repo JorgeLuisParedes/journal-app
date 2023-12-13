256846674a2f2517f30a0a15e1146ce7e3b20d85
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2023-12-13 16:14:07 +0100

    formulario de registro de usuarios

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,12 +1,26 @@
 import { Link as RouterLink } from 'react-router-dom';
-import { Google } from '@mui/icons-material';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../layout';
+import { useForm } from '../../hooks';
+
+const formData = {
+	email: 'paredesjorgeluis@gmail.com',
+	password: '123456',
+	displayName: 'Jorge Luis Paredes',
+};
 
 export const RegisterPage = () => {
+	const { displayName, email, password, onInputChange, formState } =
+		useForm(formData);
+
+	const onSubmit = event => {
+		event.preventDefault();
+		console.log(formState);
+	};
+
 	return (
 		<AuthLayout title='Crear una cuenta'>
-			<form>
+			<form onSubmit={onSubmit}>
 				<Grid container>
 					<Grid item xs={12} sx={{ mt: 2 }}>
 						<TextField
@@ -14,6 +28,9 @@ export const RegisterPage = () => {
 							type='text'
 							placeholder='Nombre completo'
 							fullWidth
+							name='displaName'
+							value={displayName}
+							onChange={onInputChange}
 						/>
 					</Grid>
 					<Grid item xs={12} sx={{ mt: 2 }}>
@@ -22,6 +39,9 @@ export const RegisterPage = () => {
 							type='email'
 							placeholder='correo@google.com'
 							fullWidth
+							name='email'
+							value={email}
+							onChange={onInputChange}
 						/>
 					</Grid>
 					<Grid item xs={12} sx={{ mt: 2 }}>
@@ -30,12 +50,15 @@ export const RegisterPage = () => {
 							type='password'
 							placeholder='Contraseña'
 							fullWidth
+							name='password'
+							value={password}
+							onChange={onInputChange}
 						/>
 					</Grid>
 
 					<Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
 						<Grid item xs={12}>
-							<Button variant='contained' fullWidth>
+							<Button type='submit' variant='contained' fullWidth>
 								Crear cuenta
 							</Button>
 						</Grid>
