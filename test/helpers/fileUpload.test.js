840b733a7a4f6d2c95b0f9eb6aab3f78fa4f0a80
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2023-12-21 13:30:40 +0100

    delete image

--- test/helpers/fileUpload.test.js
+++ test/helpers/fileUpload.test.js
@@ -1,4 +1,12 @@
+import { v2 as cloudinary } from 'cloudinary';
 import { fileUpload } from '../../src/helpers/fileUpload';
+
+cloudinary.config({
+	cloud_name: 'dxl2vuiee',
+	api_key: '487253474356563',
+	api_secret: '17MQofRPxtfRmDoH7s4teNpZqFg',
+	secure: true,
+});
 
 describe('Pruebas en fileUpload', () => {
 	test('Debe de subir el archivo correctamente en cloudinary', async () => {
@@ -10,5 +18,22 @@ describe('Pruebas en fileUpload', () => {
 
 		const url = await fileUpload(file);
 		expect(typeof url).toBe('string');
+
+		const segments = url.split('/');
+		const imageId = segments[segments.length - 1].replace('.webp', '');
+
+		const cloudResp = await cloudinary.api.delete_resources(
+			[`journal/${imageId}`],
+			{
+				resource_type: 'image',
+			},
+		);
+	});
+
+	test('debe de retornar null', async () => {
+		const file = new File([], 'foto.jpg');
+
+		const url = await fileUpload(file);
+		expect(url).toBe(null);
 	});
 });
