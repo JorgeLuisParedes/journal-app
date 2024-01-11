d3ce7049b1fb0a09e2e9c060a786b8fa575a3924
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-01-12 00:55:26 +0100

    pruebas en login

---
+++ test/auth/pages/LoginPage.test.jsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import { LoginPage } from '../../../src/auth/pages/LoginPage';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { authSlice } from '../../../src/store/auth';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Pruebas en <LoginPage />', () => {
+	const store = configureStore({
+		reducer: {
+			auth: authSlice.reducer,
+		},
+	});
+
+	test('Debe de mostrar el componente correctamente', () => {
+		render(
+			<Provider store={store}>
+				<MemoryRouter>
+					<LoginPage />
+				</MemoryRouter>
+			</Provider>,
+		);
+
+		expect(screen.getAllByText('Login').length).toBeGreaterThanOrEqual(1);
+	});
+});
